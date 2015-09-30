c91077cf5d86895a51b84a6e9250fddd9bff60af
Alexander Noriega <lambdatoast@gmail.com>
2015-09-29 23:09:33 -0300

    Rename type alias for clarity. Use many1 for accesses.

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,14 +3,14 @@ module Parser where
 import Text.Parsec as P
 import Control.Applicative 
 
-type Key = String
+type PropName = String
 type VarName = String
-data PropAccessPath = PropAccessPath  VarName [Key] deriving (Show)
+data PropAccessPath = PropAccessPath  VarName [PropName] deriving (Show)
 
 propAccessParser :: Parsec String st PropAccessPath
 propAccessParser = PropAccessPath <$> (varName <* char '.') <*> accesses
   where
     varName = many1 $ noneOf ['.']
-    accesses = (P.many $ noneOf ['.']) `sepBy` (char '.')
+    accesses = (P.many1 $ noneOf ['.']) `sepBy` (char '.')
 
 test p = parse p ""
