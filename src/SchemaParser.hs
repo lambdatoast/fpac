bcafd9811e79654459d2e5a62efaa00c1cb9a6ef
Alexander Noriega <lambdatoast@gmail.com>
2015-10-01 23:20:03 -0300

    Extend schema parsing power.

--- src/SchemaParser.hs
+++ src/SchemaParser.hs
@@ -1,16 +1,39 @@
 module SchemaParser (schemaParser) where
 
+import qualified Data.List as L
 import Text.Parsec as P
 import Control.Applicative ((<$>),(<*), (*>), (<*>), pure) 
 
 type Parser = Parsec String ()
-data Prop = Simple String String | Complex String Prop
-data Doc = Doc [Prop] 
+data Val = SVal String | OVal [Prop] deriving (Show)
+data Prop = Simple String Val deriving (Show)
+data Schema = Schema [Prop] deriving (Show)
 
-schemaParser :: Parser String
-schemaParser = 
-  P.manyTill anyChar (try $ string "new Schema") *>
-  open *> content <* close
-  where content = P.many1 $ noneOf ");"
-        open = string "("
+key :: Parser String
+key = spaces *> P.many1 alphaNum <* spaces
+
+kvSep :: Parser Char
+kvSep = spaces *> char ':' <* spaces
+
+propSep :: Parser Char
+propSep = spaces *> char ',' <* spaces
+
+simpleVal :: Parser Val
+simpleVal = SVal <$> (concat . L.intersperse "." <$> ((P.many1 alphaNum) `sepBy` char '.'))
+
+objVal :: Parser Val
+objVal = OVal <$> between (char '{') (char '}') manyProps
+
+prop :: Parser Prop
+prop = Simple <$> (key <* kvSep) <*> (try objVal <|> simpleVal)
+
+manyProps :: Parser [Prop]
+manyProps = (between spaces spaces prop) `sepBy` propSep
+
+schemaParser :: Parser Schema
+schemaParser = Schema <$>
+  (P.manyTill anyChar (try $ string "new Schema") *>
+  open *> content <* close)
+  where content = between (char '{') (char '}') manyProps
+        open = char '('
         close = string ");" <* spaces <* eof
