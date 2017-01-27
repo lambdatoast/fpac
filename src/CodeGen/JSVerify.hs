6e5124f8865b48524a36bae65f1b9dd16c158e38
Alexander Noriega <lambdatoast@gmail.com>
2017-01-27 18:31:54 +0000

    Add support for record arbs.

--- src/CodeGen/JSVerify.hs
+++ src/CodeGen/JSVerify.hs
@@ -9,16 +9,23 @@ data Arb = Number_
          | DateTime_
          | String_
          | JSON_
+         | Record_ [ArbProp]
          deriving Show
 
 newtype Name = Name Text deriving Show
 data ArbProp = ArbProp Name Arb deriving Show
 type Props = [ArbProp]
 
+recordField :: Prop -> ArbProp
+recordField (Prop s v) = ArbProp (Name s) (fromVal v)
+
 fromVal :: Val -> Arb
 fromVal (SVal "String")  = String_
 fromVal (SVal "Boolean") = Bool_
+fromVal (SVal "Number") = Number_
+fromVal (SVal "Object") = JSON_
 fromVal (OVal ((Prop "type" (SVal "Date")):_)) = DateTime_
+fromVal (OVal props) = Record_ $ fmap recordField props
 fromVal _ = JSON_
 
 generate :: Schema -> Props
@@ -32,15 +39,17 @@ fromArb Number_   = "jsc.number"
 fromArb String_   = "jsc.string"
 fromArb Bool_     = "jsc.bool"
 fromArb DateTime_ = "jsc.datetime"
+fromArb (Record_ arbprops) = "jsc.record(" ++ showProps arbprops ++ ")"
 fromArb JSON_     = "jsc.json"
 
-showJS :: Props -> Text
-showJS arbDef = L.concat $ L.intersperse sep (showJS_ arbDef)
+braces :: Text -> Text
+braces s = "{" ++ s ++ "}"
+
+showProps :: Props -> Text
+showProps arbDef = braces $ L.concat $ L.intersperse sep (showJS_ arbDef)
   where
     sep :: Text
-    sep = ",\n"
-    -- inBrackets :: Text -> Text
-    -- inBrackets s = "{" ++ s ++ "}"
+    sep = ","
     showKV :: Name -> Text -> Text
     showKV (Name n) v = n ++ ": " ++ v
     showJS_ :: Props -> [Text]
