6fa7243ce53adf20f0cd0a48c1ecacfed0d91164
Alexander Noriega <lambdatoast@gmail.com>
2017-01-27 21:49:44 -0300

    Add support for specifying the schema and template to check as program options.

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import qualified Text.Parsec as P
 import qualified Template.Parser as TP
 import qualified Schema.Parser as SP
 import qualified Schema.Check as S
+import qualified Options as O
 
 test :: P.Parsec String () a -> String -> Either P.ParseError a
 test p = P.parse p ""
@@ -12,35 +13,46 @@ test p = P.parse p ""
 checkPropAccess :: SP.Schema -> TP.PropAccessPath -> (String, Bool)
 checkPropAccess schema (TP.PropAccessPath var xs) = (L.intercalate "." $ [var] ++ xs, S.check xs schema)
 
-loadTemplate :: IO String
-loadTemplate = readFile "./test-data/blog.html"
+loadTemplate :: String -> IO String
+loadTemplate = readFile
 
-loadSchema :: IO String
-loadSchema = readFile "./test-data/blog.schema.js"
+loadSchema :: String -> IO String
+loadSchema = readFile
 
-printTemplate :: IO ()
-printTemplate = do
-  templateSrc <- loadTemplate
+printTemplate :: String -> IO ()
+printTemplate filePath = do
+  templateSrc <- loadTemplate filePath
   putStrLn $ show $ test TP.templateParser templateSrc
 
-printSchema :: IO ()
-printSchema = do
-  schemaSrc <- loadSchema
+printSchema :: String -> IO ()
+printSchema filePath = do
+  schemaSrc <- loadSchema filePath
   putStrLn $ show $ test SP.schemaParser schemaSrc
 
-printTemplateCheck :: IO ()
-printTemplateCheck =
+printTemplateCheck :: String -> String -> IO ()
+printTemplateCheck tplFilePath schFilePath =
   do 
-    templateSrc <- loadTemplate
-    schemaSrc <- loadSchema
+    templateSrc <- loadTemplate tplFilePath
+    schemaSrc <- loadSchema schFilePath
     putStrLn $ show $ do
       propAccesses <- test TP.templateParser templateSrc
       schema <- test SP.schemaParser schemaSrc
       return $ map (checkPropAccess schema) propAccesses
 
+data MainOptions = MainOptions 
+    { optTemplate :: String
+    , optSchema :: String
+    }
+
+instance O.Options MainOptions where
+  defineOptions = pure MainOptions
+    <*> O.simpleOption "template" "./test-data/blog.html" "Path to the view template"
+    <*> O.simpleOption "schema" "./test-data/blog.schema.js" "Path to the db schema"
+
 main :: IO ()
-main = do
-  printTemplate
-  printSchema
-  printTemplateCheck
+main = O.runCommand $ \opts args -> do
+  let (template, schema) = (optTemplate opts, optSchema opts)
+  printTemplate template
+  printSchema schema
+  printTemplateCheck template schema
 
